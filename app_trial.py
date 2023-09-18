328bd3de910298c2c55db1eda6fb1e75b65cd4f6
malansale <116889452+MalanSale@users.noreply.github.com>
2023-09-18 20:16:47 +0530

    Update app_trial.py

--- app_trial.py
+++ app_trial.py
@@ -113,11 +113,12 @@ def main():
         while True:
             if getattr(ctx, "video_processor", None):
                 detected_emotion = ctx.video_processor.detected_emotion
-
+                print('Detected emotion',detected_emotion)
                 if detected_emotion and generic_mood:
                     recommended_song_uris = music_player.recommend_and_store_music(
                         detected_emotion, generic_mood
                     )
+                    print('------->',recommended_song_uris)
                     selected_track_uri, track_uris = music_player.recommend_music(
                         detected_emotion, generic_mood
                     )
