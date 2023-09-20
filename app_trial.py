d6d0e688e76e20f979c4bcbd876569bc80f6e995
malansale <116889452+MalanSale@users.noreply.github.com>
2023-09-20 23:22:38 +0530

    Update app_trial.py

--- app_trial.py
+++ app_trial.py
@@ -10,18 +10,30 @@ from face_recognition.api import face_locations
 from spotipy.oauth2 import SpotifyOAuth
 from streamlit_option_menu import option_menu
 from streamlit_webrtc import webrtc_streamer, VideoProcessorBase
-
 from player import EmotionMusicPlayer
+import os
 
-scope = "user-library-read user-read-playback-state user-modify-playback-state"
-sp = spotipy.Spotify(
-    auth_manager=SpotifyOAuth(
-        client_id=st.secrets["SPOTIPY_CLIENT_ID"],
-        client_secret=st.secrets["SPOTIPY_CLIENT_SECRET"],
-        redirect_uri=st.secrets["SPOTIPY_REDIRECT_URI"],
-        scope=scope,
-    )
-)
+SPOTIPY_CLIENT_ID = "2ae751452b6a4bb385f129aa2849e30a"
+SPOTIPY_CLIENT_SECRET = "fcae1bab1b7d4cf7b3b537ca19ffcd6d"
+os.environ["SPOTIPY_CLIENT_ID"] = SPOTIPY_CLIENT_ID
+os.environ["SPOTIPY_CLIENT_SECRET"] = SPOTIPY_CLIENT_SECRET
+
+# Use the Spotify API client credentials flow (no user authentication required)
+sp = spotipy.Spotify(auth_manager=spotipy.oauth2.SpotifyClientCredentials())
+
+query = "genre:pop"
+track_uris = sp.search(query, limit=1, type="track")
+print(track_uris)
+
+# scope = "user-library-read user-read-playback-state user-modify-playback-state"
+# sp = spotipy.Spotify(
+#     auth_manager=SpotifyOAuth(
+#         client_id=st.secrets["SPOTIPY_CLIENT_ID"],
+#         client_secret=st.secrets["SPOTIPY_CLIENT_SECRET"],
+#         redirect_uri=st.secrets["SPOTIPY_REDIRECT_URI"],
+#         scope=scope,
+#     )
+# )
 company_name = "ChillTrill"
 
 
